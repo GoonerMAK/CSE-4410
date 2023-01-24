726543270ef21c0629a91ac795170e6dcf21b04d
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-01-24 19:34:15 +0600

    Lab_03

--- Lab_03/queries.sql
+++ Lab_03/queries.sql
@@ -86,5 +86,162 @@ group by movie.mov_title);
 
 
 
--- 3
+-- 3    
 
+CREATE OR REPLACE FUNCTION get_movie_earnings (id IN NUMBER)
+RETURN NUMBER
+AS
+v_total_earnings NUMBER;
+v_num_reviews NUMBER;
+v_release_date DATE;
+v_current_date DATE;
+v_mov_id NUMBER;
+BEGIN
+    SELECT COUNT(*) as num_reviews, m.mov_id
+    INTO v_num_reviews, v_mov_id
+    FROM rating r, Movie m
+    WHERE r.mov_id = m.mov_id AND r.mov_id = id AND r.rev_stars >= 6
+    group by m.mov_id;
+
+    SELECT m.mov_releasedate INTO v_release_date
+    FROM movie m
+    WHERE m.mov_id = id;
+
+    v_current_date := SYSDATE;
+
+    v_total_earnings := v_num_reviews * 10;
+
+    RETURN v_num_reviews/((v_current_date - v_release_date)/365);
+END;
+/
+    
+SELECT get_movie_earnings(901) FROM DUAL;         /* Without user input */
+
+
+/* With user input */
+DECLARE
+id number;
+BEGIN
+id:= '&id';                                      
+dbms_output.put_line(get_movie_earnings(id));
+end;
+/
+
+
+SELECT COUNT(*) as times , m.mov_id
+FROM rating r, movie m
+WHERE m.mov_id = r.mov_id AND r.rev_stars >= 6
+GROUP BY m.mov_id
+ORDER BY mov_id DESC;
+
+SELECT mov_id, mov_title, mov_releasedate from movie Order by mov_releasedate desc;
+
+
+
+-- 4
+
+CREATE OR REPLACE FUNCTION get_genre_status (id IN NUMBER)
+RETURN VARCHAR2
+AS
+    gen_title VARCHAR2(20);
+    review_count NUMBER;
+    avg_rating NUMBER(5,3);
+    Genre_Status VARCHAR2(20);
+    avg_reviews number(5,3);
+    avg_rev_stars number(5,3);
+BEGIN
+
+    /* Calculating the average reviews across all genres */
+    Select floor(Sum(total_reviews)/count(total_reviews)) INTO avg_reviews   
+    from (SELECT g.GEN_TITLE, COUNT(r.REV_ID) as total_reviews
+    FROM RATING r
+    JOIN MTYPE mt ON r.MOV_ID = mt.MOV_ID
+    JOIN GENRES g ON mt.GEN_ID = g.GEN_ID
+    GROUP BY g.GEN_TITLE);
+
+    /* Calculating the average reviews given by the reviewers */
+    SELECT AVG(NVL(REV_STARS, 0)) INTO avg_rev_stars FROM RATING;
+
+
+    SELECT GEN_TITLE, COUNT(RATING.REV_ID), AVG(RATING.REV_STARS)
+    INTO gen_title, review_count, avg_rating
+    FROM GENRES , RATING , MTYPE 
+    where GENRES.GEN_ID = MTYPE.GEN_ID AND MTYPE.MOV_ID = RATING.MOV_ID AND GENRES.GEN_ID = MTYPE.gen_id AND  id = GENRES.GEN_ID
+    GROUP BY GEN_TITLE;
+
+    IF( review_count > avg_reviews ) THEN 
+        IF ( avg_rating < avg_rev_stars ) THEN Genre_Status := 'Widely Watched';
+        ELSIF ( avg_rating > avg_rev_stars ) THEN Genre_Status := 'People''s Favorite';  
+        END IF;
+    
+    ELSIF ( review_count < avg_reviews  AND  avg_rating > avg_rev_stars ) THEN Genre_Status := 'Highly Rated';
+
+    ELSE Genre_Status := 'So So';
+    END IF; 
+
+    RETURN 'Genre: ' || gen_title || '   Reivew_Count: ' || review_count || '   Average_Rating: ' || avg_rating
+            || '    Status: ' || Genre_Status;
+END;
+/
+
+SELECT get_genre_status(1001) FROM DUAL;         /* Without user input */
+
+DECLARE
+id number;
+BEGIN
+id:= '&id';                                       /* With user input */
+dbms_output.put_line(get_genre_status(id));
+end;
+/
+
+
+SELECT GEN_TITLE, COUNT(RATING.REV_ID), AVG(RATING.REV_STARS)
+FROM GENRES , RATING , MTYPE 
+where GENRES.GEN_ID = MTYPE.GEN_ID AND MTYPE.MOV_ID = RATING.MOV_ID AND GENRES.GEN_ID = MTYPE.gen_id
+GROUP BY GEN_TITLE;
+
+
+Select floor(Sum(total_reviews)/count(total_reviews)) as Average_Reviews 
+from (SELECT g.GEN_TITLE, COUNT(r.REV_ID) as total_reviews
+FROM RATING r
+JOIN MTYPE mt ON r.MOV_ID = mt.MOV_ID
+JOIN GENRES g ON mt.GEN_ID = g.GEN_ID
+GROUP BY g.GEN_TITLE);
+
+
+SELECT AVG(NVL(REV_STARS, 0)) FROM RATING;
+
+
+
+-- 5
+
+
+CREATE OR REPLACE FUNCTION get_most_frequent_genre (p_start_date DATE, p_end_date DATE)
+RETURN VARCHAR2
+AS
+  v_genre VARCHAR2(20);
+  v_count NUMBER;
+BEGIN
+  SELECT g.gen_title, COUNT(*) as genre_count
+  INTO v_genre, v_count
+  FROM movie m, mtype mt, genres g
+  where  m.mov_id = mt.mov_id AND mt.gen_id = g.gen_id AND m.mov_releasedate 
+  BETWEEN p_start_date AND p_end_date
+  GROUP BY g.gen_title
+  ORDER BY v_count DESC;
+
+  RETURN v_genre || ' (' || v_count || ')';
+END;
+/
+
+SELECT get_most_frequent_genre(TO_DATE('01-JAN-2020', 'DD-MON-YYYY'), TO_DATE('01-JAN-2021', 'DD-MON-YYYY')) 
+FROM DUAL;
+
+SELECT g.gen_title, COUNT(*) as genre_count
+  FROM movie m, mtype mt, genres g
+  where  m.mov_id = mt.mov_id AND mt.gen_id = g.gen_id AND m.mov_releasedate 
+  BETWEEN TO_DATE('01-JAN-1940', 'DD-MON-YYYY') AND TO_DATE('01-JAN-2021', 'DD-MON-YYYY')
+  GROUP BY g.gen_title
+  ORDER BY genre_count DESC;
+
+  
